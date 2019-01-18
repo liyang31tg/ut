971582cd946603cdf37c15158a01ba1a11eb973e
yy <yy@yydeiMac.local>
2019-01-18 11:02:44 +0800

    fix bug

--- sign.go
+++ sign.go
@@ -1,45 +1,50 @@
 package ut
 
 import (
-	"crypto/sha1"
- 	"crypto/sha256"
- 	"fmt"
-	"os"
 	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"fmt"
 	"io"
+	"os"
 )
 
 func SignSha1(str string) string {
 	s := sha1.New()
 	s.Write([]byte(str))
-	return fmt.Sprintf("%x",s.Sum(nil))
+	return fmt.Sprintf("%x", s.Sum(nil))
 }
 
 func SignSha256(str string) string {
 	s := sha256.New()
 	s.Write([]byte(str))
-	return fmt.Sprintf("%x",s.Sum(nil))
+	return fmt.Sprintf("%x", s.Sum(nil))
 }
 
+func SignMd5(str string) string {
+	s := md5.New()
+	s.Write([]byte(str))
+	return fmt.Sprintf("%x", s.Sum(nil))
+}
 
-func Md5File(file string) (string,error){
+func Md5File(file string) (string, error) {
 	f, err := os.Open(file)
 	if err != nil {
-		return "" ,err
+		return "", err
 	}
 	defer f.Close()
 
 	h := md5.New()
 	if _, err := io.Copy(h, f); err != nil {
-		return "",err
+		return "", err
 	}
-	return fmt.Sprintf("%x", h.Sum(nil)),nil
+	return fmt.Sprintf("%x", h.Sum(nil)), nil
 }
 
-func Md5FileReader(r io.Reader) (string,error){
+func Md5FileReader(r io.Reader) (string, error) {
 	h := md5.New()
 	if _, err := io.Copy(h, r); err != nil {
-		return "",err
+		return "", err
 	}
-	return fmt.Sprintf("%x", h.Sum(nil)),nil
+	return fmt.Sprintf("%x", h.Sum(nil)), nil
 }
